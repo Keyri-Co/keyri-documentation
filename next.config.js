bb1276a82e3d4c0912ae018ab755405b725f4aea
Denver23 <denver9523@gmail.com>
2023-01-12 15:30:10 +0200

    Feat: make redirects from old one-level urls to new multi-level urls

--- next.config.js
+++ next.config.js
@@ -3,4 +3,34 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  async redirects() {
+    return [
+      {
+        source: '/:item(\\ios|device-fingerprinting-ux-and-fraud|android|react-native|flutter)',
+        destination: '/mobile-sdks/:item',
+        permanent: true,
+      },
+      {
+        source: '/:item(\\amazon-cognito|auth0|firebase|banno-digital-banking|supabase|ping-identity|alphapoint-apex-system)',
+        destination: '/integrations/:item',
+        permanent: true,
+      },
+      {
+        source: '/firebase-server-side-authentication',
+        destination: '/integrations/firebase/firebase-server-side-authentication',
+        permanent: true,
+      },
+      {
+        source: '/:item(\\web-app-integration|mobile-app-setup)',
+        destination: '/integrations/alphapoint-apex-system/:item',
+        permanent: true,
+      },
+      {
+        source: '/:item(\\ios-app-integration|android-app-integration)',
+        destination: '/integrations/alphapoint-apex-system/mobile-app-setup/:item',
+        permanent: true,
+      },
+    ]
+  },
+})
