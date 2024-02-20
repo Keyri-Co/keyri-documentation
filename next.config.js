fb3a510ccb55c815e183ea6d7cc97b2daee2b615
zainazeem <zain.azeem@gmail.com>
2024-02-19 22:20:10 -0500

    Fix menu, start adding redirects

--- next.config.js
+++ next.config.js
@@ -1,24 +1,26 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
 module.exports = withNextra({
   async redirects() {
     return [
       {
-        source: '/:item(\\ios|device-fingerprinting-ux-and-fraud|android|react-native|flutter)',
+        source:
+          '/:item(\\ios|device-fingerprinting-ux-and-fraud|android|react-native|flutter)',
         destination: '/mobile-sdks/:item',
         permanent: true,
       },
       {
-        source: '/:item(\\amazon-cognito|auth0|firebase|banno-digital-banking|supabase|ping-identity|alphapoint-apex-system)',
+        source:
+          '/:item(\\amazon-cognito|auth0|firebase|banno-digital-banking|supabase|ping-identity|alphapoint-apex-system)',
         destination: '/integrations/:item',
         permanent: true,
       },
       {
         source: '/firebase-server-side-authentication',
-        destination: '/integrations/firebase/firebase-server-side-authentication',
+        destination: '/integrations/firebase/server-side-authentication',
         permanent: true,
       },
       {
@@ -28,9 +30,10 @@ module.exports = withNextra({
       },
       {
         source: '/:item(\\ios-app-integration|android-app-integration)',
-        destination: '/integrations/alphapoint-apex-system/mobile-app-setup/:item',
+        destination:
+          '/integrations/alphapoint-apex-system/mobile-app-setup/:item',
         permanent: true,
       },
-    ]
+    ];
   },
-})
+});
